fd07964d35ab6a84f0aa211515803b19370a8391
Vina <96754320+vinanurfauziah@users.noreply.github.com>
2022-03-02 13:48:12 +0700

    Add files via upload

--- latihan1.php
+++ latihan1.php
@@ -1,30 +1,16 @@
 <?php
-// Control Flow / Struktur Kendali  
-// Pengulangan:  
-// for
-// while
-// do.. while
-// foreach: pengulangan khusus array
-
-// for($i = 0; $i < 5; $i++){
-//     echo "Hello World!";
-// }
-// $i = 0;
-// while ( $i < 5){
-//     echo "Hello world! <br>";
-// $i++;
-// }
-$i = 0;
-do {
-    echo "Hello World! <br>";
-$i++;
-} while ($i < 5);
-
-
-// Pengkondisian:
-// if.. else
-// if.. else if.. else
+// Pengkodisian / Percabangan
+// if else
+// if else if else
 // ternary
 // switch
 
+$x = 10;
+if ( $x < 20 ) {
+    echo "benar";
+} else if ( $x == 20 ) {
+    echo "bingo!";
+}else {
+    echo "salah";
+}
 ?>
