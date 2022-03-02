776f7466168f6a3b2b632ff4ec97695c625c9bf1
vinanurfauziah <vinanurfauziahh@gmail.com>
2022-03-02 12:05:15 +0700

    menyelesaikan latihan1 pertemuan 2

---
+++ latihan1.php
@@ -0,0 +1,30 @@
+<?php
+// Control Flow / Struktur Kendali  
+// Pengulangan:  
+// for
+// while
+// do.. while
+// foreach: pengulangan khusus array
+
+// for($i = 0; $i < 5; $i++){
+//     echo "Hello World!";
+// }
+// $i = 0;
+// while ( $i < 5){
+//     echo "Hello world! <br>";
+// $i++;
+// }
+$i = 0;
+do {
+    echo "Hello World! <br>";
+$i++;
+} while ($i < 5);
+
+
+// Pengkondisian:
+// if.. else
+// if.. else if.. else
+// ternary
+// switch
+
+?>
